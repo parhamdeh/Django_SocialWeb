e333b074e9696018e5c133e60a3083a9e025a9ba
parhamdeh <parhamdehghan1385@gmail.com>
2025-11-28 17:17:54 +0330

    3

--- first/account/form.py
+++ first/account/form.py
@@ -1,7 +1,49 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class UserRegisterationForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control', 'placeholder':'your name: '}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class' : 'form-control', 'placeholder':'your email: '}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'form-control', 'placeholder' : 'your pssword: '}))
+    password = forms.CharField(label='password', widget=forms.PasswordInput(attrs={'class' : 'form-control'}))
+    password2 = forms.CharField(label='confrim password', widget=forms.PasswordInput(attrs={'class' : 'form-control'}))
+
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = User.objects.filter(email=email).exists()
+        if user:
+            raise ValidationError('this email already exists!')
+        return email
+    
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        user = User.objects.filter(username=username).exists()
+        if user:
+            raise ValidationError('this username already exists!')
+        return username
+    
+    def clean(self):
+        cd = super().clean()
+        password = cd.get('password')
+        password2 = cd.get('password2')
+
+        if password and password2 and password != password2:
+            raise ValidationError('password must match')
+        
+
+
+class UserLoinForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class' : 'form-control', 'placeholder':'your name: '}))
+    password = forms.CharField(label='password', widget=forms.PasswordInput(attrs={'class' : 'form-control'}))
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        user = User.objects.filter(username=username).exists()
+        if user:
+            return username
+        raise ValidationError('this username not exists!')
+    
+    
--- first/account/views.py
+++ first/account/views.py
@@ -1,32 +1,49 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .form import UserRegisterationForm
+from .form import UserRegisterationForm, UserLoinForm
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
 
 
-class RegisterView(View):
+class UserRegisterView(View):
 
-    form_class = UserRegisterationForm()
+    form_class = UserRegisterationForm
     temmplate_name = 'account/register.html'
     def get(self, request):
-        form = self.form_class
+        form = self.form_class()
         return render(request, self.temmplate_name, {'form' : form})
     
     def post(self, request):
         form = self.form_class(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            if User.objects.filter(username=cd['username']).exists():
-                messages.error(request, "این نام کاربری قبلاً ثبت شده")
-                return redirect('register:user_register')
-
-            if User.objects.filter(email=cd['email']).exists():
-                messages.error(request, "این ایمیل قبلاً ثبت شده")
-                return redirect('register:user_register')
             
             User.objects.create_user(cd['username'], cd['email'], cd['password'])
             messages.success(request, 'succes register!', 'succes')
 
             return redirect('home:home')
         return render(request, self.temmplate_name, {'form' : form})
+
+
+class UserLoginView(View):
+    form_class = UserLoinForm
+    template_name = 'account/login.html'
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form' : form})
+
+
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username = cd['username'], password= cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'succes login!', 'succes')
+                return redirect('home:home')
+            messages.error(request, 'password is wrong', 'warning')
+            
+        return render(request, self.template_name, {'form': form})
